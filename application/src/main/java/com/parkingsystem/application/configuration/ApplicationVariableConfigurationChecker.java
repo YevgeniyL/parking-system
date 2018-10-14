8993de4829e0ca0a5cbda317d2dc404a9dbab8b6
Steklorez <johnnykforever@gmail.com>
2018-10-14 23:10:55 +0300

    Throw error if spring context configuration not contain settings for application (like price engine settings or other)

---
+++ application/src/main/java/com/parkingsystem/application/configuration/ApplicationVariableConfigurationChecker.java
@@ -0,0 +1,30 @@
+package com.parkingsystem.application.configuration;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ApplicationVariableConfigurationChecker implements ApplicationContextInitializer {
+    private final List<String> systemProps = Arrays.asList(
+            "price.tariff",
+            "price.roundInterval",
+            "price.minimalAmount",
+            "price.minimalAmountForCredit");
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        Set<String> notFoundSet = systemProps
+                .stream()
+                .filter(prop -> !applicationContext.getEnvironment().containsProperty(prop))
+                .collect(Collectors.toSet());
+
+        if (notFoundSet.size() > 0) {
+            throw new RuntimeException("\nAny variables need to setup in application.properties: \n" + StringUtils.arrayToDelimitedString(notFoundSet.toArray(), ", \n"));
+        }
+    }
+}
